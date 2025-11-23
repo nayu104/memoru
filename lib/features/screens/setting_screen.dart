77d5ee4a10ad6c57fe294fb52664382e028c3991
nayu104 <ose794@gmail.com>
2025-11-23 23:02:38 +0900

    refactor: update README and comment out unused import in SettingScreen

--- lib/features/screens/setting_screen.dart
+++ lib/features/screens/setting_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:memomemo/core/constants/app_urls.dart';
-import 'package:memomemo/core/app_colors.dart';
+//import 'package:memomemo/core/app_colors.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memomemo/core/provider/memo_state.dart';
 
