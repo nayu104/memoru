7b08fd0fda9a5d8a6e4817260b41a9d6d3c02d19
nayu104 <ose794@gmail.com>
2026-05-13 22:57:34 +0900

    chore: GitHub Pagesワークフローにpublicフォルダのコピーステップを追加

--- lib/core/constants/app_urls.dart
+++ lib/core/constants/app_urls.dart
@@ -1,5 +1,5 @@
 class AppUrls {
   static const String contactForm =
       'https://docs.google.com/forms/d/e/1FAIpQLSeSaK47aha1UtVZdAKEACvY45e4Wi1ERmFrGpArUZSqt9-P8A/viewform?usp=dialog';
-  static const String privacyPolicy = 'https://example.com/privacy';
+  static const String privacyPolicy = 'https://nayu104.github.io/memoru/privacy.html';
 }
